0dc1435187aa8162acaf3b8902af2981a75a0f28
edanur20 <115996375+edanur20@users.noreply.github.com>
2026-04-16 14:43:35 +0200

    Update metro.config.js

--- frontend/metro.config.js
+++ frontend/metro.config.js
@@ -1,1 +1,8 @@
+const { getDefaultConfig } = require('expo/metro-config');
 
+const config = getDefaultConfig(__dirname);
+
+// Ajouter les extensions audio/video aux assets
+config.resolver.assetExts.push('mp4', 'mp3', 'wav', 'm4a');
+
+module.exports = config;
