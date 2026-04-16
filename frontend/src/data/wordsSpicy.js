43b6a567c44dbee30c55f6b33a8acb4948376aae
edanur20 <115996375+edanur20@users.noreply.github.com>
2026-04-16 14:52:41 +0200

    Update wordsSpicy.js

--- frontend/src/data/wordsSpicy.js
+++ frontend/src/data/wordsSpicy.js
@@ -1,1 +1,311 @@
+// Mots Spicy pour Code Names - Mode adulte/vulgaire
+// 25+ catégories avec 25+ mots chacune pour garantir 100+ parties uniques
+// Règle: maximum 1 mot par catégorie par grille de 25 mots
 
+export const spicyCategories = {
+  drogues: {
+    name: 'Drogues & Substances',
+    emoji: '🌿',
+    words: [
+      'BEUH', 'JOINT', 'SHIT', 'CAME', 'SPEED', 'TAZ', 'KETA', 'CRACK', 'OPIUM', 'COKE',
+      'RAIL', 'BANG', 'TRIP', 'DEALER', 'CARTEL', 'PABLO', 'SNIFF', 'SPLIFF', 'BEDO', 'BUZZ',
+      'STONE', 'SEVRAGE', 'CHAMPIS', 'ACIDE', 'ECSTA', 'HASCH', 'GANJA', 'WEED', 'DEFONCE', 'SHOOTE',
+      'FUMETTE', 'BONG', 'DOUILLE', 'GRAMME', 'POCHON', 'TEUF', 'RAVE', 'AFTER', 'DESCENTE', 'PARANO'
+    ]
+  },
+  sexe: {
+    name: 'Sexe & Anatomie',
+    emoji: '🔥',
+    words: [
+      'ORGASME', 'FETICHE', 'BONDAGE', 'LATEX', 'FOUET', 'MENOTTES', 'STRING', 'CAPOTE', 'SEXTOY', 'STRIP',
+      'POLE', 'ESCORT', 'GIGOLO', 'NYMPHO', 'LIBIDO', 'TANTRIQUE', 'KAMASUTRA', 'ECHANGISTE', 'COQUIN', 'VOYEUR',
+      'EXHIB', 'DOMINA', 'SOUMIS', 'VIBREUR', 'PLUG', 'JOUIR', 'SUCON', 'QUICKIE', 'LEVRETTE', 'AMAZONE',
+      'BRANLETTE', 'CUNNI', 'TETONS', 'CLITO', 'PRELIMINAIRES', 'GEMISSEMENT', 'MISSIONNAIRE', 'GORGE', 'FACIAL', 'CREAMPIE'
+    ]
+  },
+  insultes: {
+    name: 'Insultes & Vulgaire',
+    emoji: '🤬',
+    words: [
+      'CONNARD', 'ENFOIRE', 'SALOPE', 'BORDEL', 'MERDE', 'BATARD', 'CHIASSE', 'GERBE', 'MORPION', 'TEIGNE',
+      'BLAIREAU', 'TOCARD', 'BOUFFON', 'BALTRINGUE', 'ABRUTI', 'DEBILE', 'CRETIN', 'COUILLON', 'ANDOUILLE', 'BRANLEUR',
+      'GLAND', 'BOLOSS', 'CASSOS', 'MINABLE', 'RATE', 'NAZE', 'RINGARD', 'PLOUC', 'BOUSEUX', 'TACHE',
+      'BOULET', 'CRADO', 'MORVEUX', 'CREVARD', 'RADIN', 'POUCAVE', 'BALANCE', 'FUMIER', 'ORDURE', 'VERMINE'
+    ]
+  },
+  intello: {
+    name: 'Mots Rares & Intello',
+    emoji: '🧠',
+    words: [
+      'ANALPHABETE', 'LOBOTOMIE', 'KLEPTOMANE', 'MYTHOMANE', 'PYROMANE', 'SOCIOPATHE', 'SCROTUM', 'PROSTATE', 'RECTUM', 'SPHINCTER',
+      'FISTULE', 'PUSTULE', 'FURONCLE', 'GANGRENE', 'CHANCRE', 'HEMORROIDES', 'PROLAPSUS', 'FLATULENCE', 'INCONTINENCE', 'NARCOLEPSIE',
+      'AMNESIE', 'DEMENCE', 'SCHIZO', 'BIPOLAIRE', 'PARANOIA', 'HYSTERIE', 'NEVROSE', 'PSYCHOSE', 'PHOBIE', 'HYPOCONDRIAQUE',
+      'MISANTHROPE', 'MEGALO', 'NARCISSIQUE', 'MASOCHISTE', 'SADIQUE', 'CYNIQUE', 'NIHILISTE', 'DESPOTE', 'TYRAN', 'DEMAGOGUE'
+    ]
+  },
+  morbide: {
+    name: 'Morbide & Glauque',
+    emoji: '💀',
+    words: [
+      'MORGUE', 'CADAVRE', 'CERCUEIL', 'CREMATION', 'EMBAUMEUR', 'FOSSOYEUR', 'CORBILLARD', 'CIMETIERE', 'CRYPTE', 'MAUSOLEE',
+      'EPITAPHE', 'VEUVE', 'ORPHELIN', 'DEUIL', 'TESTAMENT', 'HERITAGE', 'URNE', 'CENDRES', 'OSSUAIRE', 'SQUELETTE',
+      'AUTOPSIE', 'LEGISTE', 'AGONIE', 'RALE', 'LINCEUL', 'SUAIRE', 'PUTREFACTION', 'DECOMPOSITION', 'ASTICOTS', 'CHARNIER',
+      'NECROLOGIE', 'OBSEQUES', 'VEILLÉE', 'CAVEAU', 'STELE', 'TOMBE', 'DEFUNT', 'TREPASSE', 'MACABRE', 'FUNESTE'
+    ]
+  },
+  alcool: {
+    name: 'Alcool & Soirée',
+    emoji: '🍺',
+    words: [
+      'VODKA', 'WHISKY', 'RHUM', 'TEQUILA', 'GIN', 'BIERE', 'CHAMPAGNE', 'MOJITO', 'COCKTAIL', 'SHOT',
+      'CULSEC', 'TOURNEE', 'APERO', 'PINTE', 'DEMI', 'PICHET', 'MAGNUM', 'IVRE', 'BOURRE', 'PETE',
+      'TORCHE', 'DECHIRE', 'FRACASSE', 'EXPLOSE', 'BLINDE', 'ARRACHE', 'GUEULE', 'ASPIRINE', 'VOMI', 'CUVETTE',
+      'KEBAB', 'BLACKOUT', 'REGRET', 'BAFOUILLE', 'TITUBE', 'DEGUEULE', 'MURGE', 'BITURE', 'PICOLE', 'GNOLE'
+    ]
+  },
+  argent: {
+    name: 'Argent & Vice',
+    emoji: '🎰',
+    words: [
+      'CASINO', 'POKER', 'ROULETTE', 'JACKPOT', 'BANDIT', 'ARNAQUE', 'ESCROC', 'PONZI', 'PYRAMIDE', 'BLANCHIMENT',
+      'OFFSHORE', 'PANAMA', 'EVASION', 'FRAUDE', 'CORRUPTION', 'DESSOUS', 'MALLETTE', 'CASH', 'LIASSE', 'BIFFETON',
+      'THUNE', 'FRIC', 'POGNON', 'OSEILLE', 'BLE', 'MAILLE', 'MAGOT', 'CAGNOTTE', 'BUTIN', 'RANCON',
+      'RACKET', 'USURIER', 'DETTE', 'FAILLITE', 'HUISSIER', 'SAISIE', 'CREANCIER', 'INTERETS', 'PRET', 'HYPOTHEQUE'
+    ]
+  },
+  crime: {
+    name: 'Crime & Mafia',
+    emoji: '🔪',
+    words: [
+      'MAFIA', 'PARRAIN', 'CAPO', 'SICARIO', 'YAKUZA', 'TRIADE', 'CARTEL', 'GANG', 'CREW', 'CLAN',
+      'OMERTA', 'VENDETTA', 'CONTRAT', 'EXECUTION', 'REGLEMENT', 'INTIMIDATION', 'PROTECTION', 'EXTORSION', 'BRAQUAGE', 'CASSE',
+      'COFFRE', 'FOURGUE', 'RECEL', 'INDIC', 'TAUPE', 'PLANQUE', 'CAVALE', 'FUGITIF', 'RECHERCHE', 'PRIME',
+      'TRUAND', 'VOYOU', 'CRAPULE', 'CANAILLE', 'BANDIT', 'BRIGAND', 'CRIMINEL', 'RECIDIVISTE', 'PERPETUITE', 'MITARD'
+    ]
+  },
+  fluides: {
+    name: 'Corps & Fluides',
+    emoji: '💉',
+    words: [
+      'SPERME', 'CYPRINE', 'MENSTRUES', 'FECES', 'URINE', 'VOMI', 'MUCUS', 'PUS', 'SUEUR', 'SMEGMA',
+      'CERUMEN', 'BILE', 'LYMPHE', 'SALIVE', 'MORVE', 'CRACHAT', 'GLAIRE', 'SECRETIONS', 'EXCREMENT', 'DEJECTION',
+      'DIARRHEE', 'COLIQUE', 'GASTRO', 'TOURISTA', 'INDIGESTION', 'REFLUX', 'RENVOI', 'ERUCTATION', 'PET', 'PROUT',
+      'GAZ', 'BALLONNEMENT', 'GARGOUILLIS', 'HOQUET', 'ETERNUEMENT', 'TOUX', 'GLAVIOT', 'MOLLARD', 'SAIGNEMENT', 'HEMORRAGIE'
+    ]
+  },
+  maladies: {
+    name: 'Maladies & Dégueu',
+    emoji: '🦠',
+    words: [
+      'SYPHILIS', 'GONORRHEE', 'CHLAMYDIA', 'HERPES', 'CONDYLOME', 'GALE', 'TEIGNE', 'MYCOSE', 'VERRUE', 'PANARIS',
+      'DURILLON', 'COR', 'OIGNON', 'AMPOULE', 'CLOQUE', 'BRULURE', 'SUINTANT', 'PURULENT', 'INFECTE', 'NECROSE',
+      'AMPUTE', 'MOIGNON', 'PROTHESE', 'BEQUILLE', 'GRABATAIRE', 'INCONTINENT', 'COUCHE', 'SONDE', 'CATHETER', 'PERFUSION',
+      'PIQURE', 'AIGUILLE', 'SERINGUE', 'PRELEVEMENT', 'BIOPSIE', 'SCANNER', 'TOUCHER', 'RECTAL', 'COLOSCOPIE', 'LAVEMENT'
+    ]
+  },
+  bestioles: {
+    name: 'Bestioles & Parasites',
+    emoji: '🐛',
+    words: [
+      'CAFARD', 'BLATTE', 'CANCRELAT', 'MOUSTIQUE', 'MOUCHE', 'ASTICOT', 'LARVE', 'VER', 'TENIA', 'ASCARIS',
+      'OXYURE', 'TIQUE', 'PUCE', 'PUNAISE', 'ARAIGNEE', 'SCORPION', 'MILLEPATTES', 'SCOLOPENDRE', 'GUEPE', 'FRELON',
+      'SANGSUE', 'LIMACE', 'RAT', 'SOURIS', 'MULOT', 'PIGEON', 'GOELAND', 'MOUETTE', 'CORBEAU', 'CHAROGNARD',
+      'VAUTOUR', 'HYENE', 'MOUFETTE', 'PUTOIS', 'FOUINE', 'NUISIBLE', 'VERMINE', 'RAMPANT', 'GROUILLANT', 'INFESTE'
+    ]
+  },
+  bouffe: {
+    name: 'Nourriture Douteuse',
+    emoji: '🍆',
+    words: [
+      'TRIPES', 'ANDOUILLETTE', 'BOUDIN', 'ROGNONS', 'CERVELLE', 'LANGUE', 'OREILLES', 'MUSEAU', 'PIEDS', 'QUEUE',
+      'TESTICULES', 'COUILLES', 'HUITRES', 'OURSIN', 'BULOT', 'BIGORNEAU', 'ESCARGOT', 'GRENOUILLE', 'CUISSES', 'FOIE',
+      'GRAS', 'RIS', 'MOELLE', 'OS', 'CARTILAGE', 'TENDON', 'NERF', 'COUENNE', 'LARD', 'SAINDOUX',
+      'GRAILLON', 'FRITURE', 'RANCE', 'MOISI', 'PERIME', 'AVARIE', 'POURRI', 'FERMENTE', 'FAISANDE', 'TOURNE'
+    ]
+  },
+  medical: {
+    name: 'Médical & Chirurgie',
+    emoji: '🏥',
+    words: [
+      'SCALPEL', 'BISTOURI', 'SUTURE', 'AGRAFES', 'DRAIN', 'STENT', 'PACEMAKER', 'DEFIBRILLATEUR', 'REA', 'COMA',
+      'INTUBE', 'VENTILE', 'DIALYSE', 'GREFFE', 'DONNEUR', 'REJET', 'CHIMIO', 'RADIOTHERAPIE', 'METASTASE', 'TUMEUR',
+      'KYSTE', 'POLYPE', 'ABLATION', 'MASTECTOMIE', 'HYSTERECTOMIE', 'VASECTOMIE', 'CIRCONCISION', 'EPISIOTOMIE', 'CESARIENNE', 'FORCEPS',
+      'VENTOUSE', 'PERIDURALE', 'CONTRACTIONS', 'DILATATION', 'DELIVRANCE', 'PLACENTA', 'CORDON', 'INCUBATEUR', 'PREMATURE', 'NEONAT'
+    ]
+  },
+  genants: {
+    name: 'Situations Gênantes',
+    emoji: '🎭',
+    words: [
+      'FLAGRANT', 'SURPRIS', 'GRILLE', 'CRAME', 'DEMASQUE', 'CONFONDU', 'HUMILIE', 'RIDICULISE', 'MOQUE', 'HUE',
+      'SIFFLE', 'VIRE', 'DEGAGE', 'LARGUE', 'TROMPE', 'COCU', 'CORNES', 'FRIENDZONE', 'RATEAU', 'VENT',
+      'VESTE', 'HONTE', 'GENE', 'MALAISE', 'AWKWARD', 'CRINGE', 'FLOP', 'BIDE', 'FOUR', 'FIASCO',
+      'DESASTRE', 'CATASTROPHE', 'NAUFRAGE', 'CRASH', 'FAIL', 'EPIC', 'LOOSE', 'OWNED', 'REKT', 'DESTROYED'
+    ]
+  },
+  cirque: {
+    name: 'Cirque & Bizarre',
+    emoji: '🎪',
+    words: [
+      'FREAK', 'MONSTRE', 'PHENOMENE', 'ANOMALIE', 'DIFFORMITE', 'CONTORSION', 'DISLOCATION', 'AVALER', 'SABRES', 'CRACHEUR',
+      'FAKIR', 'CLOUS', 'BRAISES', 'CHARMEUR', 'SERPENTS', 'BARBE', 'TATOUE', 'NAIN', 'GEANT', 'SIAMOIS',
+      'ALBINOS', 'TELEPATHIE', 'VOYANCE', 'HYPNOSE', 'TRANSE', 'POSSESSION', 'EXORCISME', 'VAUDOU', 'POUPEE', 'MALEDICTION',
+      'SORTILEGE', 'ENVOUTEMENT', 'MARABOUT', 'CHARLATAN', 'GOUROU', 'SECTE', 'ADEPTE', 'LAVAGE', 'CERVEAU', 'MANIPULATION'
+    ]
+  },
+  toxique: {
+    name: 'Relations Toxiques',
+    emoji: '💔',
+    words: [
+      'GHOSTING', 'BENCHING', 'GASLIGHTING', 'LOVEBOMBING', 'CATFISH', 'STALKER', 'HARCELEUR', 'PERVERS', 'MANIPULATEUR', 'POSSESSIF',
+      'JALOUX', 'CONTROLANT', 'ETOUFFANT', 'DEPENDANT', 'CODEPENDANT', 'FUSIONNEL', 'TOXIQUE', 'MALSAIN', 'DESTRUCTEUR', 'VIOLENT',
+      'AGRESSIF', 'BOUDEUR', 'RANCUNIER', 'VENGEUR', 'MESQUIN', 'MECHANT', 'CRUEL', 'HUMILIANT', 'RABAISSANT', 'DENIGRANT',
+      'INSULTANT', 'MEPRISANT', 'CONDESCENDANT', 'HAUTAIN', 'ARROGANT', 'INFIDELE', 'MENTEUR', 'TROMPEUR', 'LACHE', 'FUYANT'
+    ]
+  },
+  vulgaire: {
+    name: 'Expressions Vulgaires',
+    emoji: '🌶️',
+    words: [
+      'NIQUER', 'BAISER', 'FOUTRE', 'CHIER', 'PISSER', 'CRACHER', 'GERBER', 'DEGUEULASSE', 'DEGLINGUE', 'DEFONCE',
+      'EXPLOSE', 'NIQUE', 'BAISE', 'FOUTU', 'MERDIQUE', 'POURRI', 'MOISI', 'CRADE', 'CRACRA', 'DEGUEU',
+      'IMMONDE', 'INFECT', 'INFAME', 'IGNOBLE', 'ABJECT', 'REPUGNANT', 'ECOEURANT', 'VOMITIF', 'NAUSEABOND', 'FETIDE',
+      'PUTRIDE', 'PESTILENTIEL', 'MEPHITIQUE', 'DELETERE', 'TOXIQUE', 'NOCIF', 'NUISIBLE', 'MALFAISANT', 'MALEFIQUE', 'DIABOLIQUE'
+    ]
+  },
+  prison: {
+    name: 'Prison & Justice',
+    emoji: '🔒',
+    words: [
+      'TAULE', 'ZONZON', 'CELLULE', 'BARREAUX', 'MATON', 'GARDIEN', 'DETENU', 'PRISONNIER', 'TAULARD', 'CONDAMNE',
+      'SENTENCE', 'VERDICT', 'PROCES', 'TRIBUNAL', 'JUGE', 'AVOCAT', 'PROCUREUR', 'TEMOIN', 'ACCUSE', 'COUPABLE',
+      'INNOCENT', 'ACQUITTE', 'APPEL', 'CASSATION', 'SURSIS', 'FERME', 'BRACELET', 'ELECTRONIQUE', 'PROBATION', 'LIBERTE',
+      'CONDITIONNELLE', 'EVASION', 'FUGUE', 'MENOTTES', 'FOUILLE', 'PARLOIR', 'PROMENADE', 'CANTINE', 'MITARD', 'ISOLEMENT'
+    ]
+  },
+  tabac: {
+    name: 'Tabac & Addiction',
+    emoji: '🚬',
+    words: [
+      'CLOPE', 'CIGARETTE', 'TABAC', 'NICOTINE', 'GOUDRON', 'FUMEE', 'CENDRIER', 'MEGOT', 'FILTRE', 'ROULEE',
+      'BRIQUET', 'ALLUMETTE', 'PAQUET', 'CARTOUCHE', 'BURALISTE', 'TOUSSEUR', 'CRACHAT', 'POUMON', 'CANCER', 'EMPHYSEME',
+      'ADDICTION', 'DEPENDANCE', 'SEVRAGE', 'PATCH', 'GOMME', 'VAPOTEUSE', 'ECIG', 'CHICHA', 'NARGILE', 'HOOKAH',
+      'PASSIVE', 'FUMOIR', 'TERASSE', 'PAUSE', 'GRILLER', 'TAPER', 'TAXER', 'QUEMANDER', 'ACCRO', 'INTOXIQUE'
+    ]
+  },
+  corps: {
+    name: 'Corps Tabou',
+    emoji: '🍑',
+    words: [
+      'FESSES', 'CUL', 'DERRIERE', 'POSTÉRIEUR', 'POPOTIN', 'BOULE', 'NICHONS', 'SEINS', 'POITRINE', 'LOCHES',
+      'TETONS', 'MAMELONS', 'PARTIES', 'INTIMES', 'BIJOUX', 'FAMILLE', 'ENGIN', 'OUTIL', 'PAQUET', 'SERVICE',
+      'ZIGOUNETTE', 'ZIZI', 'BITE', 'QUEUE', 'PENIS', 'TESTICULES', 'COUILLES', 'BURNES', 'VAGIN', 'CHATTE',
+      'VULVE', 'PUBIS', 'POILS', 'RASAGE', 'EPILATION', 'MAILLOT', 'INTEGRAL', 'BRESILIEN', 'TICKET', 'METRO'
+    ]
+  },
+  toilettes: {
+    name: 'Toilettes & WC',
+    emoji: '🚽',
+    words: [
+      'CHIOTTES', 'TOILETTES', 'CABINETS', 'LATRINES', 'TRONE', 'CUVETTE', 'LUNETTE', 'ABATTANT', 'CHASSE', 'PAPIER',
+      'PQ', 'BROSSE', 'DEBOUCHEUR', 'VENTOUSE', 'PLOMBIER', 'BOUCHON', 'DEBORDEMENT', 'INONDATION', 'ODEUR', 'DESODORISANT',
+      'SPRAY', 'ALLUMETTE', 'INTIMITE', 'OCCUPE', 'VERROU', 'GRAFFITI', 'AUTOROUTE', 'PUBLIQUES', 'PAYANTES', 'TURQUES',
+      'SECHE', 'MAINS', 'DISTRIBUTEUR', 'SAVON', 'HYGIENE', 'PROPRETE', 'SALETE', 'DEGEULASSE', 'IMMONDE', 'NETTOYER'
+    ]
+  },
+  horreur: {
+    name: 'Horreur & Gore',
+    emoji: '🩸',
+    words: [
+      'SANG', 'GORE', 'TRIPAILLE', 'BOYAUX', 'ENTRAILLES', 'VISCERES', 'ORGANES', 'MEMBRE', 'AMPUTATION', 'DECAPITATION',
+      'EVISCERATION', 'ECORCHEMENT', 'TORTURE', 'SUPPLICE', 'AGONIE', 'SOUFFRANCE', 'DOULEUR', 'HURLEMENT', 'CRI', 'TERREUR',
+      'EFFROI', 'PANIQUE', 'CAUCHEMAR', 'MONSTRE', 'CREATURE', 'DEMON', 'SPECTRE', 'REVENANT', 'ZOMBIE', 'VAMPIRE',
+      'LOUP-GAROU', 'SORCIERE', 'MALEDICTION', 'POSSEDE', 'EXORCISME', 'ENFER', 'DAMNE', 'PURGATOIRE', 'APOCALYPSE', 'CATACLYSME'
+    ]
+  },
+  porno: {
+    name: 'Industrie Adulte',
+    emoji: '🔞',
+    words: [
+      'PORNO', 'ADULT', 'XXX', 'RATED', 'EXPLICIT', 'UNCENSORED', 'HARDCORE', 'SOFTCORE', 'AMATEUR', 'PROFESSIONNEL',
+      'ACTEUR', 'ACTRICE', 'REALISATEUR', 'PRODUCTEUR', 'STUDIO', 'TOURNAGE', 'SCENE', 'POSITION', 'CATEGORIE', 'FETISH',
+      'MILF', 'TEEN', 'MATURE', 'COUGAR', 'DADDY', 'STEPMOM', 'STEPSIS', 'CASTING', 'COUCH', 'AUDITION',
+      'WEBCAM', 'LIVE', 'STREAM', 'CHAT', 'PRIVATE', 'SHOW', 'TIP', 'TOKEN', 'PREMIUM', 'VIP'
+    ]
+  },
+  scato: {
+    name: 'Scatologique',
+    emoji: '💩',
+    words: [
+      'CACA', 'CROTTE', 'ETRON', 'COLOMBIN', 'MOULAGE', 'FLOTTEUR', 'TORPILLE', 'BOUDIN', 'SAUCISSE', 'CHIURE',
+      'FIENTE', 'BOUSE', 'CROTTIN', 'FUMIER', 'PURIN', 'LISIER', 'FOSSE', 'SEPTIQUE', 'EPANDAGE', 'COMPOST',
+      'DECOMPOSITION', 'FERMENTATION', 'METHANE', 'PESTILENCE', 'PUANTEUR', 'RELENT', 'EFFLUVES', 'MIASMES', 'REMUGLES', 'EXHALAISONS',
+      'CONSTIPE', 'DIARRHEIQUE', 'COLITE', 'LAVEMENT', 'PURGE', 'LAXATIF', 'SUPPO', 'INTESTIN', 'COLON', 'RECTUM'
+    ]
+  },
+  insultesFamille: {
+    name: 'Insultes Familiales',
+    emoji: '👨‍👩‍👧',
+    words: [
+      'BATARD', 'FILS', 'PUTE', 'SALOPE', 'CHIEN', 'CHIENNE', 'PORC', 'TRUIE', 'DINDE', 'POULE',
+      'COUILLE', 'MOLLE', 'COUILLON', 'CRETIN', 'DEBILE', 'MONGOL', 'ATTARDE', 'DEMEURE', 'SIMPLE', 'TARE',
+      'DEGENERE', 'CONSANGUIN', 'INCESTUEUX', 'BANCAL', 'TORDU', 'BARGE', 'DINGUE', 'CINGLÉ', 'FRAPPE', 'FELE',
+      'CASE', 'MANQUE', 'NEURONE', 'CERVEAU', 'LENT', 'BOUCHE', 'TROU', 'NEANT', 'ZERO', 'MOINS'
+    ]
+  },
+  arnaque: {
+    name: 'Arnaques & Escroqueries',
+    emoji: '🎣',
+    words: [
+      'ARNAQUE', 'ESCROQUERIE', 'FRAUDE', 'ENTOURLOUPE', 'MAGOUILLE', 'COMBINE', 'EMBROUILLE', 'ENTUBE', 'BAISE', 'PLUME',
+      'PIGEONNER', 'ROULER', 'DUPER', 'FLOUER', 'GRUGER', 'REFAIRE', 'CAROTTE', 'ENFLER', 'GONFLER', 'POMPER',
+      'SOUTIRER', 'EXTORQUER', 'RACKETTER', 'TAXER', 'PONCTIONNER', 'SAIGNER', 'PLUMER', 'DEPOUILLER', 'VOLER', 'DEROBER',
+      'SUBTILISER', 'CHIPER', 'FAUCHER', 'PIQUER', 'CHOURER', 'GRATTER', 'DETOURNER', 'ESCAMOTER', 'TRUQUER', 'FALSIFIER'
+    ]
+  }
+};
+
+// Fonction pour obtenir tous les mots spicy
+export const getAllSpicyWords = () => {
+  const allWords = [];
+  Object.values(spicyCategories).forEach(category => {
+    allWords.push(...category.words);
+  });
+  return allWords;
+};
+
+// Fonction pour générer une grille de 25 mots avec max 1 mot par catégorie
+export const generateSpicyGrid = () => {
+  const categories = Object.keys(spicyCategories);
+  const selectedWords = [];
+  const usedCategories = new Set();
+
+  // Mélanger les catégories
+  const shuffledCategories = [...categories].sort(() => Math.random() - 0.5);
+
+  // Sélectionner 25 mots de 25 catégories différentes
+  for (let i = 0; i < 25 && i < shuffledCategories.length; i++) {
+    const categoryKey = shuffledCategories[i];
+    const category = spicyCategories[categoryKey];
+    const words = category.words;
+    const randomWord = words[Math.floor(Math.random() * words.length)];
+    selectedWords.push(randomWord);
+    usedCategories.add(categoryKey);
+  }
+
+  // Si on n'a pas assez de catégories (ne devrait pas arriver avec 25+), compléter
+  while (selectedWords.length < 25) {
+    const randomCategory = categories[Math.floor(Math.random() * categories.length)];
+    const category = spicyCategories[randomCategory];
+    const words = category.words;
+    const randomWord = words[Math.floor(Math.random() * words.length)];
+    if (!selectedWords.includes(randomWord)) {
+      selectedWords.push(randomWord);
+    }
+  }
+
+  return selectedWords.sort(() => Math.random() - 0.5);
+};
+
+// Liste plate pour compatibilité avec l'ancien système
+export const wordsSpicyFr = getAllSpicyWords();
